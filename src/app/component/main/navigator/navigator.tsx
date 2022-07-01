74fa513a9e972de608591d79d1d5ab24e6ce6210
chry8822 <goggg8822@naver.com>
2022-07-01 18:36:16 +0900

    test

--- src/app/component/main/navigator/navigator.tsx
+++ src/app/component/main/navigator/navigator.tsx
@@ -8,7 +8,7 @@ const MainNavigator = () => {
           <li><a href={undefined} className="nav01 current">메인</a></li>
           <li><a href={undefined} className="nav02">간병내역</a></li>
           <li><a href={undefined} className="nav03 new">커뮤니티</a></li>
-          <li><a href={undefined} className="nav04">마이페이지</a></li>
+          <li><a href={undefined} className="nav04">오국화 </a></li>
         </ul>
       </nav>
     </>
